61284fe4a0dd2e16f214022e2f20d6a38e19f6aa
Sagar <sagar.p@inapp.com>
2024-06-05 14:46:09 +0530

    change in folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geco/login/bloc/login_bloc_bloc.dart';
+import 'package:geco/login/loginscreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiBlocProvider(providers: [
+    BlocProvider<LoginBlocBloc>(
+      create: (BuildContext context) => LoginBlocBloc(),
+    ),
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      builder: (context, child) {
+        return MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+            child: child!);
+      },
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        // '/login': (context) => BlocProvider(
+        //     create: (context) => LoginBloc(repository: UserRepository()),
+        //     child: const Login()),
+        // '/dashboard': (context) => BlocProvider(
+        //       create: (context) => DashboardBloc(),
+        //       child: const DashBoard(),
+        //     ),
+        // '/signup': (context) => const SignUp(),
+        // '/signupDetails': (context) => const SignUpUserDetails(),
+        // '/forgot_password': (context) => BlocProvider(
+        //       create: (context) => ForgotPwdBloc(repository: UserRepository()),
+        //       child: const ForgotPW(),
+        //     ),
+        // '/my_account': (context) => const MyAccountPage(),
+      },
     );
   }
 }
@@ -77,15 +86,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
